98a1199fa0a1d71fcc5fdef62f4a691ab4f1305c
Tenkho123 <100073835+Tenkho123@users.noreply.github.com>
2025-08-11 15:39:44 +0700

    minor adjustment

--- modules/LoadingScreen/loadingscreen.js
+++ modules/LoadingScreen/loadingscreen.js
@@ -3,12 +3,22 @@
 //   loadingScreen.style.display = 'none';
 // };
 
-setTimeout(function() {
+// setTimeout(function() {
+//     const loadingScreen = document.getElementById('loading-screen');
+//     loadingScreen.classList.add('fade-out');
+
+//     // fading 1 second
+//     setTimeout(() => {
+//       loadingScreen.style.display = 'none';
+//     }, 1000);
+//   }, 3000); // 3 seconds
+
+window.addEventListener("load", function() {
+  setTimeout(() => {
     const loadingScreen = document.getElementById('loading-screen');
     loadingScreen.classList.add('fade-out');
-
-    // fading 1 second
     setTimeout(() => {
       loadingScreen.style.display = 'none';
     }, 1000);
-  }, 3000); // 3 seconds
+  }, 2000); // 0.5s after load
+});
